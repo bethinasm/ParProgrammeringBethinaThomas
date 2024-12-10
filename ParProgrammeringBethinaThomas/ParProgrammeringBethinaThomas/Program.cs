d625f7909746d506fd71618a02fea4cbc1d01b7b
Thomas Helland <thomas.helland97@gmail.com>
2024-12-10 13:38:56 +0100

    Hei

--- ParProgrammeringBethinaThomas/ParProgrammeringBethinaThomas/Program.cs
+++ ParProgrammeringBethinaThomas/ParProgrammeringBethinaThomas/Program.cs
@@ -1,12 +1,54 @@
-﻿
-using ParProgrammeringBethinaThomas;
+﻿using ParProgrammeringBethinaThomas;
 
-Console.WriteLine("Par Programmering Oppgave\n");
-Console.WriteLine("TO DO APP");
-/*
-    Konsollbasert ToDo liste
-    Lag en todo app! Du skal kunne legge til oppgaver, slette dem og gå inn på en task for å se beskrivelse eller flere detaljer
- */
+class Program
+{
+    static void Main()
+    {
 
-var start = new Start();
-start.Menu();
+        Console.WriteLine("Par Programmering Oppgave\n");
+        Console.WriteLine("TO DO APP");
+        /*
+            Konsollbasert ToDo liste
+            Lag en todo app! Du skal kunne legge til oppgaver, slette dem og gå inn på en task for å se beskrivelse eller flere detaljer
+         */
+
+        Start start = new Start();
+
+
+        do
+        {
+            Console.WriteLine("What would you like to do? \n[1] Add task \n[2] Delete task \n[3] Show all tasks \n[4] Exit program\n\n");
+            var userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "1":
+                    Console.Clear();
+                    start.AddTask();
+                    break;
+                case "2":
+                    Console.Clear();
+                    start.DeleteTask();
+                    break;
+                case "3":
+                    Console.Clear();
+                    start.ShowAllTasks();
+                    break;
+                case "4":
+                    Console.Clear();
+                    Console.WriteLine("The program will now shut down.");
+                    Thread.Sleep(1500);
+                    return;
+                //Environment.Exit(0);
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Not a valid input, try again.");
+                    Thread.Sleep(1500);
+                    break;
+            }
+        } while (true);
+    }
+
+}
+
+
--- ParProgrammeringBethinaThomas/ParProgrammeringBethinaThomas/Start.cs
+++ ParProgrammeringBethinaThomas/ParProgrammeringBethinaThomas/Start.cs
@@ -1,60 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ParProgrammeringBethinaThomas
+﻿namespace ParProgrammeringBethinaThomas
 {
     internal class Start
     {
-        public void Menu()
+
+        private List<Task> toDoList = new List<Task>
         {
-            bool isRunning = true;
+            new Task("Walk the dog"," 11.12 .24", "Go for a walk in the forest with the dog"),
+            new Task("Feed the cats", "10.12.24", "Chicken or something")
+        };
 
-            while (isRunning)
-            {
-                Console.WriteLine("What would you like to do? \n[1] Add task \n[2] Delete task \n[3] Show all tasks \n[4] Exit program\n\n");
-                var userInput = Console.ReadLine();
+        // public void Menu()
+        // {
+        //     bool isRunning = true;
 
-                switch (userInput)
-                {
-                    case "1":
-                        Console.Clear();
-                        AddTask();
-                        break;
-                    case "2":
-                        Console.Clear();
-                        DeleteTask();
-                        break;
-                    case "3":
-                        Console.Clear();
-                        ShowAllTasks();
-                        break;
-                    case "4":
-                        isRunning = false;
-                        Console.Clear();
-                        Console.WriteLine("The program will now shut down.");
-                        Thread.Sleep(1500);
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.Clear();
-                        Console.WriteLine("Not a valid input, try again.");
-                        Thread.Sleep(1500);
-                        break;
-                }
-            }
-        }
+        //     do
+        //     {
+        //         Console.WriteLine("What would you like to do? \n[1] Add task \n[2] Delete task \n[3] Show all tasks \n[4] Exit program\n\n");
+        //         var userInput = Console.ReadLine();
 
-        private void AddTask()
+        //         switch (userInput)
+        //         {
+        //             case "1":
+        //                 Console.Clear();
+        //                 AddTask();
+        //                 break;
+        //             case "2":
+        //                 Console.Clear();
+        //                 DeleteTask();
+        //                 break;
+        //             case "3":
+        //                 Console.Clear();
+        //                 ShowAllTasks();
+        //                 break;
+        //             case "4":
+        //                 isRunning = false;
+        //                 Console.Clear();
+        //                 Console.WriteLine("The program will now shut down.");
+        //                 Thread.Sleep(1500);
+        //                 return;
+        //                 //Environment.Exit(0);
+        //             default:
+        //                 Console.Clear();
+        //                 Console.WriteLine("Not a valid input, try again.");
+        //                 Thread.Sleep(1500);
+        //                 break;
+        //         }
+        //     } while (isRunning);
+        // }
+
+        public void AddTask()
         {
             Console.WriteLine("Write the name of you task:");
-            string nameInput = Console.ReadLine();
+            var nameInput = Console.ReadLine();
             Console.WriteLine("Write the due date of your task:");
-            string dateInput = Console.ReadLine();
+            var dateInput = Console.ReadLine();
             Console.WriteLine("Give a short description of you task:");
-            string descriptionInput = Console.ReadLine();
+            var descriptionInput = Console.ReadLine();
             Console.WriteLine();
             Thread.Sleep(1000);
             toDoList.Add(new Task(nameInput, dateInput, descriptionInput));
@@ -62,18 +63,29 @@ namespace ParProgrammeringBethinaThomas
 
         }
 
-        private void DeleteTask()
+        public void DeleteTask()
         {
+            for (int i = 0; i < toDoList.Count; i++)
+            {
+                Console.WriteLine($"Index: [{i+1}]" +
+                                  $"Name: {toDoList[i].taskName}" +
+                                  $"Date: {toDoList[i].taskDate}" +
+                                  $"Description {toDoList[i].taskDescription}");
+            }
 
+            Console.WriteLine("Write the number of the index for the task you want to delete");
+
+            var input = Console.ReadLine();
+
+            int inputToInt = Convert.ToInt32(input);
+
+            int x = inputToInt - 1;
+
+            toDoList.RemoveAt(x);
         }
 
-        private List<Task> toDoList = new List<Task>
-            { 
-                new Task("Walk the dog"," 11.12 .24", "Go for a walk in the forest with the dog"), 
-                new Task("Feed the cats", "10.12.24", "Chicken or something")
-            };
 
-        private void ShowAllTasks()
+        public void ShowAllTasks()
         {
             Console.WriteLine("Showing all tasks: \n");
             foreach (var task in toDoList)
